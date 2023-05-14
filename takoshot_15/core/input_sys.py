ba0a2f1c1712d207e5d629ac02239164263a5d1f
hekotare <you@example.com>
2023-05-14 11:00:44 +0900

    tako_15

---
+++ takoshot_15/core/input_sys.py
@@ -0,0 +1,258 @@
+import enum
+
+import pygame
+from pynput import keyboard
+
+
+class KeyState:
+    Push    = 0x01 # 押した瞬間
+    Hold    = 0x02 # 押され続けている
+    Release = 0x04 # 離した瞬間
+    Free    = 0x08 # 押されていない
+
+class InputSys:
+
+    def __init__(self):
+        self._keyboard_listener = None
+        self._keystate_list = [KeyState.Free] * KeyCodes.KEY_NUM
+        self._pressed_list = [False] * KeyCodes.KEY_NUM
+        self._time_list = [0] * KeyCodes.KEY_NUM # キーを押したときの時間（フレーム）を記録しておく（キーのリピートに使う）
+        self._observe_list = [] # 現在押されているキー（監視対象とする）
+        self.mouse_pos = (0, 0)
+        self.mouse_move = (0, 0)
+        self.delta = 0
+    
+    def init(self, time_sys):
+        self._time_sys = time_sys # キーのリピート押しのために追加
+        self._keyboard_listener = keyboard.Listener(on_press=self._on_press, on_release=self._on_release)
+        self._keyboard_listener.start()
+    
+    def is_push(self, vk):
+        return self.is_key_state(vk, KeyState.Push)
+    
+    # repeat_frame ボタンを一定間隔でリピート押しする場合に使う
+    def is_hold(self, vk, repeat_frame = 0):
+        return self._is_repeat(vk, repeat_frame) and self.is_key_state(vk, KeyState.Hold)
+    
+    def _is_repeat(self, vk, repeat_frame = 0):
+    
+        if (repeat_frame <= 0): return True # リピート設定なし
+        
+        elapsed_frame = self._time_sys.sys_frames - self._time_list[vk]
+        
+        n = elapsed_frame % repeat_frame
+        
+        return repeat_frame <= elapsed_frame and n == 0
+    
+    def is_release(self, vk):
+        return self.is_key_state(vk, KeyState.Release)
+    
+    def is_free(self, vk):
+        return self.is_key_state(vk, KeyState.Free)
+    
+    def is_keydown(self, vk, repeat_frame = 0):
+        return self.is_push(vk) or self.is_hold(vk, repeat_frame)
+    
+    def is_keyup(self, vk):
+        return self.is_key_state(vk, KeyState.Release | KeyState.Free)
+    
+    def is_key_state(self, vk, key_state):
+        return (self._keystate_list[vk] & key_state) != 0
+    
+    def get_key_state(self, vk):
+        return self._keystate_list[vk]
+    
+    # 毎フレームの開始時に呼び出す
+    def process_key(self):
+    
+        for vk in self._observe_list[:]:
+            new_key_state = self._update_key_state(vk, self._pressed_list[vk], self._keystate_list[vk])
+            if (new_key_state == KeyState.Free):
+                self._observe_list.remove(vk) # キー状態がフリーになったので、監視対象から外す
+        
+        # とりあえずの処理でマウスも追加
+        mouse_pressed = pygame.mouse.get_pressed()
+        self._update_key_state(KeyCodes.MOUSE_LEFT,  mouse_pressed[0], self._keystate_list[KeyCodes.MOUSE_LEFT])
+        self._update_key_state(KeyCodes.MOUSE_WHEEL, mouse_pressed[1], self._keystate_list[KeyCodes.MOUSE_WHEEL])
+        self._update_key_state(KeyCodes.MOUSE_RIGHT, mouse_pressed[2], self._keystate_list[KeyCodes.MOUSE_RIGHT])
+        
+        old_mouse_pos = self.mouse_pos
+        self.mouse_pos = pygame.mouse.get_pos()
+        self.mouse_move = self.mouse_pos[0] - old_mouse_pos[0], self.mouse_pos[1] - old_mouse_pos[1]
+    
+    def end_process(self):
+        self.delta = 0
+
+    # とりあえずこんなんでいい？
+    # 
+    # マウスホイールのスクロール量を調べる方法がよくわからなかったので、
+    # 外部でマウスイベントをチェックし、スクロール量をセットするようにした
+    def on_mousewheel_delta(self, delta):
+        self.delta += delta
+    
+    def _update_key_state(self, vk, pressed, old_key_state):
+    
+        if pressed:
+            if (old_key_state & (KeyState.Free | KeyState.Release)) != 0:
+                new_key_state = KeyState.Push
+            else:
+                new_key_state = KeyState.Hold # ボタンを押し続けている
+        else:
+            if (old_key_state & (KeyState.Push | KeyState.Hold)) != 0:
+                new_key_state = KeyState.Release
+            else:
+                new_key_state = KeyState.Free
+        
+        self._keystate_list[vk] = new_key_state
+        
+        return new_key_state
+    
+    def _on_press(self, key):
+        vk = self._key_to_vk(key)
+        #print(f'on_press vk={vk}')
+        
+        if (self._pressed_list[vk] == False):
+            self._pressed_list[vk] = True
+            
+            self._time_list[vk] = self._time_sys.sys_frames
+            
+            # 観察リストに登録、不要かもしれんけど一応重複チェック
+            if vk not in self._observe_list:
+                self._observe_list.append(vk)
+
+    def _on_release(self, key):
+        vk = self._key_to_vk(key)
+        #print(f'on_release vk={vk}')
+        
+        if (self._pressed_list[vk] == True):
+            self._pressed_list[vk] = False
+    
+    #
+    # キー入力イベントからキーの値を得る
+    #
+    # 特殊キー（shift、スペース、escキーなど）を押したときはEnum型が渡される
+    # pynputの_event_to_keyあたりが原因とおもうけどこれは正常な動作？バグ？
+    #
+    # enum型のときは、enum.value.vkの値を得るようにする
+    def _key_to_vk(self, key):
+        if isinstance(key, enum.Enum):
+            return key.value.vk
+        else:
+            return key.vk
+    
+
+# キーコードの定義（これでいいんかな？）
+class KeyCodes:
+    BACKSPACE = 8
+    TAB = 9
+    ENTER = 13
+                    
+    #SHIFT = 16
+    LSHIFT = 160
+    RSHIFT = 161
+    
+    CTRL = 17
+    PAUSE = 19
+    CAPSLOCK = 20
+    ESC = 27
+    SPACE = 32
+                    
+    PAGEUP = 33
+    PAGEDOWN = 34
+    END = 35
+    HOME = 36
+                    
+    LEFT = 37
+    UP = 38
+    RIGHT = 39
+    DOWN = 40
+                    
+    INSERT = 45
+    DELETE = 46
+                    
+    KEY_0 = 48
+    KEY_1 = 49
+    KEY_2 = 50
+    KEY_3 = 51
+    KEY_4 = 52
+    KEY_5 = 53
+    KEY_6 = 54
+    KEY_7 = 55
+    KEY_8 = 56
+    KEY_9 = 57
+                    
+    A = 65
+    B = 66
+    C = 67
+    D = 68
+    E = 69
+    F = 70
+    G = 71
+    H = 72
+    I = 73
+    J = 74
+    K = 75
+    L = 76
+    M = 77
+    N = 78
+    O = 79
+    P = 80
+    Q = 81
+    R = 82
+    S = 83
+    T = 84
+    U = 85
+    V = 86
+    W = 87
+    X = 88
+    Y = 89
+    Z = 90
+    
+    NUMPAD0 = 96
+    NUMPAD1 = 97
+    NUMPAD2 = 98
+    NUMPAD3 = 99
+    NUMPAD4 = 100
+    NUMPAD5 = 101
+    NUMPAD6 = 102
+    NUMPAD7 = 103
+    NUMPAD8 = 104
+    NUMPAD9 = 105
+    MULTIPLY = 106
+    ADD = 107
+                    
+    SUBTRACT = 109
+    DECIMAL = 110
+    DIVIDE = 111
+                    
+    F1 = 112
+    F2 = 113
+    F3 = 114
+    F4 = 115
+    F5 = 116
+    F6 = 117
+    F7 = 118
+    F8 = 119
+    F9 = 120
+    F10 = 121
+    F11 = 122
+    F12 = 123
+                    
+    NUMLOCK = 144
+    SCROLL_LOCK = 145
+    
+    COLON = 186
+    PLUS = 187 # ;+
+    COMMA = 188 # ,<
+    MINUS = 189 # -=
+    PERIOD = 190 # .>
+    QUESTION = 191 # ?
+    OPEN_BRACKET = 219
+    CLOSED_BRACKET = 221
+    UNDER_SCORE = 226
+
+    MOUSE_LEFT = 256
+    MOUSE_WHEEL = 257
+    MOUSE_RIGHT = 258
+                    
+    KEY_NUM = 260
---
+++ takoshot_15/entity/shell.py
@@ -0,0 +1,89 @@
+import pygame
+from defines import ShellType
+
+class Shell:
+
+    @classmethod
+    def create_shell(cls, shell_type, owner, world, x, y, velocity):
+        
+        if (shell_type == ShellType.WARP):
+            return Shell_Warp(shell_type, owner, world, x, y, velocity)
+        else:
+            return Shell(shell_type, owner, world, x, y, velocity)
+        
+        assert False, f"shell_type={type}"
+        
+        return None
+    
+    def __init__(self, type, owner, world, x, y, velocity):
+        self.type = type
+        self.owner = owner
+        self.world = world
+        self.x, self.y = x, y
+        self.radius, self.damage = 20, 20
+        self.velocity = velocity
+        self.old_rotate_deg = 0 # 一部の弾でグラフィック描画用に使う
+        self.remain_velocity = pygame.Vector2()
+        self.kill = False
+    
+    def is_kill(self):
+        return self.kill
+    
+    def update(self):
+    
+        if (self.kill): return
+        
+        if self.world.collision.check_shell_lost(self.x, self.y):
+            self.kill = True
+            return
+        
+        # 移動（なんとなくx, yを整数値にしておく）
+        vx, vy = self.velocity + self.remain_velocity
+        vx_i, vy_i = int(vx), int(vy)
+        self.remain_velocity.update(vx - vx_i, vy - vy_i)
+        x = self.x + vx_i; y = self.y + vy_i
+
+        hit, x, y = self.world.collision.intersect_ground_and_line_segment(self.x, self.y, vx_i, vy_i)
+        
+        if (hit):
+            self.on_land(x, y)
+        
+        self.x = x
+        self.y = y
+        
+        # テキトーに物理的な計算
+        self.velocity = (self.velocity[0] * 0.996, # 空気抵抗
+                            self.velocity[1] + 0.125) # 重力
+
+    # 弾が地面に当たった！
+    def on_land(self, x, y):
+        # 地形を破壊する
+        breaked_list = []
+        self.world.collision.break_block_in_circle(x, y, self.radius, breaked_list)
+
+        self.kill = True
+    
+    def hittest(self, x, y, radius, attack_rate):
+        # radius内のキャラクターにダメージを与える
+        for collide_data in self.world.collision.intersect_circle_and_players(x, y, radius, self.world.player_list):
+        
+            player = collide_data['player']
+            d = collide_data['dist']
+            
+            # 爆発の中心に近いほど数値が大きくなる(0.0～1.0)
+            damage_rate = 1 - (d / radius)
+            damage_rate *= 1.75 # 距離が少し離れただけでダメージが小さくなるので、ダメージを出やすくする
+            damage_rate = g.clamp(damage_rate, 0, 1)
+
+            # ダメージ(2～)
+            damage = max(int(self.damage * attack_rate * damage_rate), 2)
+            # ダメージイベントを送信
+            player.on_you_have_damaged(damage)
+
+# ワープ弾の処理
+class Shell_Warp(Shell):
+    # 弾が地面に着弾した時の処理
+    def on_land(self, x, y):
+        # ワープ弾を撃ったプレイヤーを移動させる
+        self.owner.x, self.owner.y = x, y
+        self.kill = True
